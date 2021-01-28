90d239f7949764cda1292c2d86c3feafb613e165
Edwardwmd <1732141816wmd@gmail.com>
2021-01-28 18:23:05 +0800

    展示数据

--- app/src/main/java/com/edw/inputbuttonview/MainActivity.java
+++ app/src/main/java/com/edw/inputbuttonview/MainActivity.java
@@ -3,23 +3,32 @@ package com.edw.inputbuttonview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements InputButtonView.onCountChangeListener {
 
     private InputButtonView inputButtonView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inputButtonView = findViewById(R.id.ipt_button);
-        inputButtonView.setOnCountChangeListener(this);
+        inputButtonView.setBtnHeight(50);
+        inputButtonView.setBtnWidth(250);
+        inputButtonView.setMax(20);
+        inputButtonView.setMin(-20);
     }
 
     @Override
     public void onCountChange(int count) {
-        Toast.makeText(this, "单价:2元一个------>总价为" + 2 * count + "元", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
@@ -28,4 +37,5 @@ public class MainActivity extends AppCompatActivity implements InputButtonView.o
         inputButtonView.destroyView();
         inputButtonView = null;
     }
+
 }
